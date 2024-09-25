94ba9d381d55d5b59ab3bc6a0c0d58bdb3472a00
AllenSabu <94732272+InfinityDemon2001@users.noreply.github.com>
2024-09-25 22:05:34 +0530

    .

---
+++ L40/index.js
@@ -0,0 +1,41 @@
+// var age = 25;
+
+// if(true) {
+//     console.log(age);
+// }
+
+// function solve() {
+//     var age = 25;
+
+//     console.log(age);
+// }
+
+// console.log(age);
+// solve();
+
+// var x = 10;
+// var x = 20;
+
+// {
+//     let a = 10;
+
+//     console.log(a);
+// }
+
+
+// let a = 20;
+// a = 30;
+
+// let a = 10;
+// a = "allen";
+// a = true;
+// a = null;
+
+const a = 28;
+console.log(a);
+
+// const a = 98;
+// console.log(a);
+
+let marks = "allen";
+console.log(typeof(marks));
